4e18fa088096c7f3b3f7941389f21a849368ebcb
T. E. Pickering <te.pickering@gmail.com>
2017-03-31 13:08:09 -0700

    sbigcam.py - add support for ST-402 which may become the MATCam with the ST-I becoming the RATCam

--- sbigclient/sbigcam.py
+++ sbigclient/sbigcam.py
@@ -25,6 +25,7 @@ class CCDCam(indiclient):
         self.debug = debug
         if not self.connected:
             self.connect()
+            time.sleep(1)
 
         # run this to clear any queued events
         self.process_events()
@@ -82,91 +83,6 @@ class CCDCam(indiclient):
     def object(self, string):
         o_vec = self.set_and_send_text(self.driver, "FITS_HEADER", "FITS_OBJECT", string)
 
-    def connect(self):
-        """
-        Enable camera connection
-        """
-        vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CONNECTION", "Connect")
-        if self.debug:
-            vec.tell()
-        self.process_events()
-        return vec
-
-    def disconnect(self):
-        """
-        Disable camera connection
-        """
-        vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CONNECTION", "Disconnect")
-        if self.debug:
-            vec.tell()
-        return vec
-
-    def _default_def_handler(self, vector, indi):
-        """
-        Overload the default vector handler to do a vector.tell() so it's clear what's going on
-        """
-        if self.debug:
-            vector.tell()
-        pass
-
-    def expose(self, exptime=1.0, exptype="Light"):
-        """
-        Take exposure and return FITS data
-        """
-        if exptype not in ["Light", "Dark", "Bias", "Flat"]:
-            raise Exception("Invalid exposure type, %s. Must be one of 'Light', 'Dark', 'Bias', or 'Flat'." % exptype)
-
-        if exptime < 0.0 or exptime > 3600.0:
-            raise Exception("Invalid exposure time, %f. Must be >= 0 and <= 3600 sec." % exptime)
-
-        ft_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CCD_FRAME_TYPE", exptype)
-        if self.debug:
-            ft_vec.tell()
-
-        exp_vec = self.set_and_send_float(self.driver, "CCD_EXPOSURE", "CCD_EXPOSURE_VALUE", exptime)
-        if self.debug:
-            exp_vec.tell()
-
-        self.defvectorlist = []
-        fitsdata = None
-        run = True
-
-        while run:
-            self.process_receive_vector_queue()
-            while self.receive_event_queue.empty() is False:
-                vector = self.receive_event_queue.get()
-                if vector.tag.get_type() == "BLOBVector":
-                    print("reading out...")
-                    blob = vector.get_first_element()
-                    if blob.get_plain_format() == ".fits":
-                        buf = io.BytesIO(blob.get_data())
-                        fitsdata = fits.open(buf)
-                    run = False
-                    break
-            time.sleep(0.1)
-        return fitsdata
-
-
-class MATCam(CCDCam):
-    """
-    Wrap CCDCam, set the driver to the SBIG driver, and point to the server for the MAT camera.
-    """
-    def __init__(self, host="sbig-srv.mmto.arizona.edu", port=7624):
-        super(MATCam, self).__init__(host, port, driver="SBIG CCD")
-        self.observer = "Mount Alignment Telescope"
-        self.process_events()
-
-
-class F9WFSCam(CCDCam):
-    """
-    Wrap CCDCam, set the driver to the SBIG driver, and point to the server for the F9WFS camera.
-    """
-    def __init__(self, host="sbig-srv.mmto.arizona.edu", port=7625):
-        super(F9WFSCam, self).__init__(host, port, driver="SBIG CCD")
-        self.observer = "F/9 WFS"
-        self.process_events()
-        self.wfs_config()
-
     @property
     def temperature(self):
         t = self.get_float(self.driver, "CCD_TEMPERATURE", "CCD_TEMPERATURE_VALUE")
@@ -220,6 +136,184 @@ class F9WFSCam(CCDCam):
             if bindict['Y'] >= 1:
                 y_vec = self.set_and_send_float(self.driver, "CCD_BINNING", "VER_BIN", int(bindict['Y']))
 
+    def connect(self):
+        """
+        Enable camera connection
+        """
+        vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CONNECTION", "Connect")
+        if self.debug:
+            vec.tell()
+        self.process_events()
+        return vec
+
+    def disconnect(self):
+        """
+        Disable camera connection
+        """
+        vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CONNECTION", "Disconnect")
+        if self.debug:
+            vec.tell()
+        return vec
+
+    def _default_def_handler(self, vector, indi):
+        """
+        Overload the default vector handler to do a vector.tell() so it's clear what's going on
+        """
+        if self.debug:
+            vector.tell()
+        pass
+
+    def cooling_on(self):
+        """
+        Turn the cooler on
+        """
+        c_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CCD_COOLER", "On")
+        return c_vec
+
+    def cooling_off(self):
+        """
+        Turn the cooler off
+        """
+        c_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CCD_COOLER", "Off")
+        return c_vec
+
+    def expose(self, exptime=1.0, exptype="Light"):
+        """
+        Take exposure and return FITS data
+        """
+        if exptype not in ["Light", "Dark", "Bias", "Flat"]:
+            raise Exception("Invalid exposure type, %s. Must be one of 'Light', 'Dark', 'Bias', or 'Flat'." % exptype)
+
+        if exptime < 0.0 or exptime > 3600.0:
+            raise Exception("Invalid exposure time, %f. Must be >= 0 and <= 3600 sec." % exptime)
+
+        ft_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CCD_FRAME_TYPE", exptype)
+        if self.debug:
+            ft_vec.tell()
+
+        exp_vec = self.set_and_send_float(self.driver, "CCD_EXPOSURE", "CCD_EXPOSURE_VALUE", exptime)
+        if self.debug:
+            exp_vec.tell()
+
+        self.defvectorlist = []
+        fitsdata = None
+        run = True
+
+        while run:
+            self.process_receive_vector_queue()
+            while self.receive_event_queue.empty() is False:
+                vector = self.receive_event_queue.get()
+                if vector.tag.get_type() == "BLOBVector":
+                    print("reading out...")
+                    blob = vector.get_first_element()
+                    if blob.get_plain_format() == ".fits":
+                        buf = io.BytesIO(blob.get_data())
+                        fitsdata = fits.open(buf)
+                    run = False
+                    break
+            time.sleep(0.1)
+        return fitsdata
+
+
+class RATCam(CCDCam):
+    """
+    Wrap CCDCam, set the driver to the SBIG driver, and point to the server for the RAT camera, a monochrome ST-IM
+    """
+    def __init__(self, host="sbig-srv.mmto.arizona.edu", port=7624):
+        super(MATCam, self).__init__(host, port, driver="SBIG CCD")
+        self.observer = "Rotator Alignment Telescope"
+        self.process_events()
+
+    # turn off a bunch of functionality that doesn't apply to the ST-I series
+    @property
+    def temperature(self):
+        return None
+
+    @property
+    def cooling_power(self):
+        return None
+
+    @property
+    def cooler(self):
+        return None
+
+    @property
+    def fan(self):
+        return None
+
+    def cooling_on(self):
+        pass
+
+    def cooling_off(self):
+        pass
+
+
+class MATCam(CCDCam):
+    """
+    Wrap CCDCam, set the driver to the SBIG driver, and point to the server for the MAT camera, an ST-402 with BVR filters
+    """
+    def __init__(self, host="sbig-srv.mmto.arizona.edu", port=7624):
+        super(MATCam, self).__init__(host, port, driver="SBIG CCD")
+        self.observer = "Mount Alignment Telescope"
+        self.process_events()
+
+        # enable filter wheel
+        self.enable_cfw()
+        time.sleep(1)
+
+        self.filters = {
+            'R': 1,
+            'V': 2,
+            'B': 3,
+            'Clear': 4
+        }
+
+        # set filter labels
+        for f in self.filters:
+            self.set_and_send_text(self.driver, "FILTER_NAME", "FILTER_SLOT_NAME_%d" % self.filters[f], f)
+
+    @property
+    def filter(self):
+        slot = int(self.get_float(self.driver, "FILTER_SLOT", "FILTER_SLOT_VALUE"))
+        for k, i in self.filters.items():
+            if i == slot:
+                return k
+        return None
+
+    @filter.setter
+    def filter(self, f):
+        if isinstance(f, int):
+            if f >= 1 and f <= 4:
+                for k, i in self.filters.items():
+                    if i == f:
+                        self.object = k
+                self.set_and_send_float(self.driver, "FILTER_SLOT", "FILTER_SLOT_VALUE", f)
+        else:
+            if f in self.filters:
+                self.object = f
+                self.set_and_send_float(self.driver, "FILTER_SLOT", "FILTER_SLOT_VALUE", self.filters[f])
+
+    def enable_cfw(self):
+        type_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CFW_TYPE", "CFW-402")
+        cfw_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CFW_CONNECTION", "Connect")
+
+        return cfw_vec, type_vec
+
+    def disable_cfw(self):
+        cfw_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CFW_CONNECTION", "Disconnect")
+        return cfw_vec
+
+
+class F9WFSCam(CCDCam):
+    """
+    Wrap CCDCam, set the driver to the SBIG driver, and point to the server for the F9WFS camera.
+    """
+    def __init__(self, host="sbig-srv.mmto.arizona.edu", port=7625):
+        super(F9WFSCam, self).__init__(host, port, driver="SBIG CCD")
+        self.observer = "F/9 WFS"
+        self.process_events()
+        self.wfs_config()
+
     def fan_on(self):
         """
         Turn the fan on (DISABLED due to bug in SBIG library)
@@ -235,20 +329,6 @@ class F9WFSCam(CCDCam):
         #f_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CCD_FAN", "Off")
         f_vec = None
         return f_vec
-
-    def cooling_on(self):
-        """
-        Turn the cooler on
-        """
-        c_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CCD_COOLER", "On")
-        return c_vec
-
-    def cooling_off(self):
-        """
-        Turn the cooler off
-        """
-        c_vec = self.set_and_send_switchvector_by_elementlabel(self.driver, "CCD_COOLER", "Off")
-        return c_vec
 
     def default_config(self):
         """
